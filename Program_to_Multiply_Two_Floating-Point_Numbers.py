fbbc127474ebdc9fc050b4cd2be8010331551ed0
Omteja Yallapragada <119728012+omteja04@users.noreply.github.com>
2023-05-17 20:15:17 +0530

    # https://thecodemind.io/app/discription.php?pageCategory=c3pzTm1NaHFsYWVCeFpGMVpkTDloZz09&Tid=bCt3WlAvSHJ0UFZNQ1lYanRHWm5lQT09&Pid=ZUhnMTVZdzRINFpMSnFmVW1iYTQwQT09&Course=eUdCU2wyQVJUZGx4TjltZnFMNEtyZz09&Technology=L2RYTEdBRUMwdnh0Uzh5N0NlMU1iUT09&Topic=SXVxYURxU2Y1N2VDd2E3d1hpVExMZz09 num1=float(input()) num2=float(input()) print(round(num1*num2,2))

---
+++ Program_to_Multiply_Two_Floating-Point_Numbers.py
@@ -0,0 +1,4 @@
+# https://thecodemind.io/app/discription.php?pageCategory=c3pzTm1NaHFsYWVCeFpGMVpkTDloZz09&Tid=bCt3WlAvSHJ0UFZNQ1lYanRHWm5lQT09&Pid=ZUhnMTVZdzRINFpMSnFmVW1iYTQwQT09&Course=eUdCU2wyQVJUZGx4TjltZnFMNEtyZz09&Technology=L2RYTEdBRUMwdnh0Uzh5N0NlMU1iUT09&Topic=SXVxYURxU2Y1N2VDd2E3d1hpVExMZz09
+num1=float(input())
+num2=float(input())
+print(round(num1*num2,2))
