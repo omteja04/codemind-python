1adb1b3e1d392b686c3590fd76b4078a21143547
Omteja Yallapragada <119728012+omteja04@users.noreply.github.com>
2023-05-17 20:15:47 +0530

    # https://thecodemind.io/app/discription.php?pageCategory=c3pzTm1NaHFsYWVCeFpGMVpkTDloZz09&Tid=bCt3WlAvSHJ0UFZNQ1lYanRHWm5lQT09&Pid=MFN1UnVsSFgxMDNJR0oyejFPYmhyUT09&Course=eUdCU2wyQVJUZGx4TjltZnFMNEtyZz09&Technology=L2RYTEdBRUMwdnh0Uzh5N0NlMU1iUT09&Topic=SXVxYURxU2Y1N2VDd2E3d1hpVExMZz09 print(ord(input()))

---
+++ Find_ASCII_Value_of_a_Character.py
@@ -0,0 +1,2 @@
+# https://thecodemind.io/app/discription.php?pageCategory=c3pzTm1NaHFsYWVCeFpGMVpkTDloZz09&Tid=bCt3WlAvSHJ0UFZNQ1lYanRHWm5lQT09&Pid=MFN1UnVsSFgxMDNJR0oyejFPYmhyUT09&Course=eUdCU2wyQVJUZGx4TjltZnFMNEtyZz09&Technology=L2RYTEdBRUMwdnh0Uzh5N0NlMU1iUT09&Topic=SXVxYURxU2Y1N2VDd2E3d1hpVExMZz09
+print(ord(input()))
